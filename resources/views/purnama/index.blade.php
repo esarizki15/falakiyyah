77ee427b33eebe8e4e27344715313822a1292dac
Esa Rizki <esa.rizki.hari.utama@altomatik.com>
2022-03-28 20:19:57 +0700

    up

--- resources/views/purnama/index.blade.php
+++ resources/views/purnama/index.blade.php
@@ -55,44 +55,8 @@
                     <div class="row">
                         <div class="col-lg-4 col-md-6">
                             <div class="form-group">
-                                <label for="markaz">Markaz</label>
-                                <input required value="{{ old('markaz') }}" class="form-control" id="markaz" name="markaz">
-                            </div>
-                        </div>
-                        <div class="col-lg-4 col-md-6">
-                            <div class="form-group">
-                                <label for="lintang">Lintang</label>
-                                <input required type="number" step="any" value="{{ old('lintang') }}" class="form-control" id="lintang" name="lintang">
-                            </div>
-                        </div>
-                        <div class="col-lg-4 col-md-6">
-                            <div class="form-group">
-                                <label for="bujur">Bujur</label>
-                                <input required type="number" step="any" value="{{ old('bujur') }}" class="form-control" id="bujur" name="bujur">
-                            </div>
-                        </div>
-                        <div class="col-lg-4 col-md-6">
-                            <div class="form-group">
-                                <label for="tinggi_tempat">Tinggi Tempat</label>
-                                <input required type="number" step="any" min="0" value="{{ old('tinggi_tempat') }}" class="form-control" id="tinggi_tempat" name="tinggi_tempat">
-                            </div>
-                        </div>
-                        <div class="col-lg-4 col-md-6">
-                            <div class="form-group">
                                 <label for="zona_waktu">Zona Waktu</label>
                                 <input required type="number" step="any" value="{{ old('zona_waktu') }}" class="form-control" id="zona_waktu" name="zona_waktu">
-                            </div>
-                        </div>
-                        <div class="col-lg-4 col-md-6">
-                            <div class="form-group">
-                                <label for="ihtiyath">Ihtiyath</label>
-                                <input required type="number" step="any" min="0" value="{{ old('ihtiyath') }}" class="form-control" id="ihtiyath" name="ihtiyath">
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="tanggal">Tanggal Ijtima'</label>
-                                <input required type="date" value="{{ ($data != null && $data['DATE_CARBON'] != null)? $data['DATE_CARBON']->toDateString() : \Carbon\Carbon::now()->toDateString() }}" class="form-control" id="tanggal" name="tanggal">
                             </div>
                         </div>
                     </div>
--- resources/views/purnama/print.blade.php
+++ resources/views/purnama/print.blade.php
@@ -2,35 +2,37 @@
 @section('content')
 <div class="row">
     <div class="col">
-        @include('partial.header-print')
-        @if(!empty($astronomical))
         <div class="row">
-            <div class="col">
-                <table class="table table-hover display nowrap text-center table-bordered" style="width:100%;">
-                    <thead>
-                        <tr>
-                            <th>Lintang</th>
-                            <th>Bujur</th>
-                            <th>Tinggi Tempat</th>
-                            <th>Zona Waktu</th>
-                            <th>Tanggal</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>{{ $astronomical['lintang'] }}</td>
-                            <td>{{ $astronomical['bujur'] }}</td>
-                            <td>{{ $astronomical['tinggi_tempat'] }}</td>
-                            <td>{{ $astronomical['zona_waktu'] }}</td>
-                            <td>{{ $astronomical['tanggal']->toDateString() }}</td>
-                        </tr>
-                    </tbody>
-                </table>
+            {{-- <div>
+                <img src="{{url('/img/logo.png')}}" alt="..."  style="position:absolute;z-index:1; left:30px; height:110px;">
+            </div> --}}
+            <div class="col text-center">
+                <div class="row" style="font-size: 18px; font-weight:bold; font-family: 'Reem Kufi', sans-serif;">
+                    <div class="col"><h1>{{ strtoupper('مؤسسة معهد دار الحق الإسلامي الإنجلي') }}</h1></div>
+                </div>
+                <div class="row mt-2" style="font-size: 18px; font-weight:bold; font-family: 'Reem Kufi', sans-serif;">
+                    <div class="col"><h4>{{ strtoupper('pondok pesantren daar el haqqi') }}</h4></div>
+                </div>
+                <div class="row mt-2" style="font-size: 18px; font-weight:bold; font-family: 'Reem Kufi', sans-serif;">
+                    <div class="col"><h4>{{ strtoupper("perhitungan purnama ramadhan ". $data['tahun_hijriah'] ." h / ". $data['THN'] ." m") }}</h4></div>
+                </div>
+                <div class="row mt-2" style="font-size: 18px; font-weight:bold; font-family: 'Reem Kufi', sans-serif;">
+                    <div class="col">Kp. & Ds. Ciakar Rt. 02/01 Jl. H. Dirman Gg. Encle, Kec. Panongan, Kab. Tangerang</div>
+                </div>
+                <hr style="border: 2px solid black;">
             </div>
         </div>
-        @endif
-        @include('print.ijtima-hasil')
-        @include('partial.ringkasan-ijtima')
+        @include('print.purnama-hasil')
+        <div class="row mt-4">
+            <div class="col text-center">
+                <span>Terjadi Purnama pada pertengahan bulan {{ $data['bulan_hijriah']['nama'] }} tahun {{ $data['tahun_hijriah'] }}H pada hari {{ $data['Hari'] }} {{ $data['Pasaran'] }} yang bertepatan dengan tanggal {{ $data['TGL'] }} {{ \App\Bulan::where('nomor', $data['BLN'])->first()->nama }} {{ $data['THN'] }} pada pukul {{ explode(" ", $data['WD'])[0] }} WIB</span>
+            </div>
+        </div>
+        <div class="row mt-3">
+            <div class="col text-center">
+                <span>والله سبحانه وتعالى أعلم</span>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
